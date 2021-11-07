b802e4ccaaa836b3688007e3933542795c4f48c3
Efe Harmankaya <eharmankaya.18@gmail.com>
2021-11-07 14:08:01 -0500

    working test edge cases when n!=s

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 # https://onestepcode.com/numpy-array-views-tic-tac-toe/
 # https://numpy.org/devdocs/reference/generated/numpy.diagonal.html
+# https://codereview.stackexchange.com/questions/24764/tic-tac-toe-victory-check
 
 import json
 import numpy as np
@@ -29,13 +30,46 @@ boards = [
         np.array(['X', '.', '.']),
         np.array(['.', 'X', '.']),
         np.array(['.', '.', 'X'])
+    ]),
+    np.array([
+        np.array(['X', '.', '.', '.']),
+        np.array(['X', '.', '.', '.']),
+        np.array(['X', '.', '.', '.']),
+        np.array(['.', '.', '.', '.']),
+    ]),
+    np.array([
+        np.array(['.', '.', '.', '.']),
+        np.array(['.', 'X', 'X', 'X']),
+        np.array(['.', '.', '.', '.']),
+        np.array(['.', '.', '.', '.']),
+    ]),
+    np.array([
+        np.array(['.', '.', '.', '.']),
+        np.array(['.', 'X', '.', '.']),
+        np.array(['.', '.', 'X', '.']),
+        np.array(['.', '.', '.', 'X']),
+    ]),
+    np.array([
+        np.array(['.', '.', '.', '.']),
+        np.array(['.', '.', 'O', '.']),
+        np.array(['.', 'O', '.', '.']),
+        np.array(['O', '.', '.', '.']),
+    ]),
+    np.array([
+        np.array(['O', 'O', 'X', '.']),
+        np.array(['.', 'X', 'X', 'X']),
+        np.array(['.', 'O', 'O', '.']),
+        np.array(['.', '.', 'O', '.']),
     ])
 ]
+output = [("X","col"),("O","col"),("X","row"),("X","diag"),("X","col"),("X","row"),("X","diag"),("O", "diag"),("X","row")]
 board_empty = np.full((n,n), '.')
 
 def is_end(board):
     print(board)
-    for i in range(n):
+
+    # working nxn len(s) tic tac toe algo
+    for i in range(len(board)):
         rows = board[i, :]
         cols = board[:, i]
         diag_1 = np.diagonal(board, i)
@@ -44,36 +78,53 @@ def is_end(board):
         print(f"col[{i}] = {cols}")
         print(f"diag_1[{i}] = {diag_1}")
         print(f"diag_2[{i}] = {diag_2}")
-        # for j in range(len(rows) - s + 1):
-        #     print("entered target loop")
-        #     if rows[j:j+s] == ["X" for _ in range(s)]:
-        #         print(f"ROWS: i = {i}, j = {j}")
-        #         return True
-        #     elif cols[j:j+s] == ['X' for _ in range(s)]:
-        #         print(f"COLS: i = {i}, j = {j}")
-        #         return True
-        #     print("===")
-        #     print(cols[j:j+s])
-        #     print("===")
-
         for player in ["X", "O"]:
-            player_win = [player for _ in range(s)] 
-            if np.all(cols == player_win): # vertical win
-                print("COLS")
-            elif np.all(rows == player_win): # horizontal win
-                print("ROWS")
-            elif (len(diag_1) >= s and np.all(diag_1 == player_win)): # length check to ensure win condition is even possible
-                print("DIAG1")
-            elif (len(diag_2) >= s and np.all(diag_2 == player_win)):
-                print("DIAG2")
-            else:
-                continue
+            player_win = np.array([player for _ in range(s)])
+            for j in range(len(rows) - s + 1):
+                if np.all(rows[j:j+s] == player_win):
+                    print(f"ROWS: i = {i}, j = {j}")
+                    return player, "row"
+                elif np.all(cols[j:j+s] == player_win):
+                    print(f"COLS: i = {i}, j = {j}")
+                    return player, "col"
+                elif len(diag_1) >= s and np.all(diag_1[j:j+s] == player_win):
+                    print("DIAG1")
+                    return player, "diag"
+                elif len(diag_2) >= s and np.all(diag_2[j:j+s] == player_win):
+                    print("DIAG2")
+                    return player, "diag"
 
-            return player # reached if a condition other than else was executed
+
+        # for player in ["X", "O"]:
+        #     player_win = np.array([player for _ in range(s)])
+        #     print(u"=" * 20)
+        #     print(cols)
+        #     print(player_win)
+        #     print(u"=" * 20)
+
+        #     out = ""
+        #     if np.all(cols == player_win): # vertical win
+        #         print("COLS")
+        #         out = "col"
+        #     elif np.all(rows == player_win): # horizontal win
+        #         print("ROWS")
+        #         out = "row"
+        #     elif (len(diag_1) >= s and np.all(diag_1 == player_win)): # length check to ensure win condition is even possible
+        #         print("DIAG1")
+        #         out = "diag"
+        #     elif (len(diag_2) >= s and np.all(diag_2 == player_win)):
+        #         print("DIAG2")
+        #         out = "diag"
+        #     else:
+        #         continue
+
+        #     return player, out # reached if a condition other than else was executed
     return False            
         
 
-# print(board_empty)
-for board in boards:
-    print(is_end(board))
+for board, result in zip(boards, output):
+    out = is_end(board)
+    print(out)
+    assert out[0] == result[0]
+    assert out[1] == result[1]
     print("========")
